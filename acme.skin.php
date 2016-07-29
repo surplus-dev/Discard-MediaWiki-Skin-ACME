87349c8395afe3f3f4bae0bfa0a686425ddb1e71
2DU <min08101@naver.com>
2016-07-29 18:26:11 +0900

    넣고

--- acme.skin.php
+++ acme.skin.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <?php 
