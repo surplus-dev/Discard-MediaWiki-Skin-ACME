18cc147ce81d4ddde1ddd02ea9ea45cde68fdbaf
Cocoa <cocoa@kiwki.us>
2016-07-30 15:47:28 +0900

    Update acme.php

--- acme.php
+++ acme.php
@@ -4,9 +4,9 @@ if ( ! defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki pack
 
 $wgExtensionCredits['skin'][] = array(
 	'path'        => __FILE__,
-	'name'        => 'acme',
+	'name'        => 'Kiwitic(acme)',
 	'url'         => 'https://shapebootstrap.net/item/1524925-acme-free-responsive-corporate-template/comments',
-	'author'      => 'cosmic 원본, 김동동 수정<br>2DU 더 수정',
+	'author'      => 'cosmic 원본, 김동동 수정<br>2DU 더 수정<br>코코아가 키위위키에 맞게 수정',
 	'description' => 'acme 부트스트랩 테마를 미디어위키에 적용합니다.',
         'license-name' => "FREE LICENSE",
         'version' => '16-07-28 일반',
