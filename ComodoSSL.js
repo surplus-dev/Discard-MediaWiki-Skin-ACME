a64526d5aa42c13ec2b3fa125a75fced74b24824
Cocoa <cocoa@kiwki.us>
2016-08-02 19:24:38 +0900

    Create ComodoSSL.js

---
+++ ComodoSSL.js
@@ -0,0 +1,9 @@
+<script type="text/javascript"> //<![CDATA[ 
+var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.comodo.com/" : "http://www.trustlogo.com/");
+document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
+//]]>
+</script>
+<script language="JavaScript" type="text/javascript">
+TrustLogo("https://kiwki.us/comodo_secure_seal.png", "CL1", "none");
+</script>
+<a  href="https://ssl.comodo.com" id="comodoTL">SSL Certificates</a>
