b414f2eb5b8ad5e6d18ce959ae188711a55c5935
Cocoa <cocoa@kiwki.us>
2016-08-16 00:25:15 +0900

    광고쪽 수정

--- dark.skin.php
+++ dark.skin.php
@@ -231,7 +231,7 @@ class DarkTemplate extends BaseTemplate {
 	<div class="row">
 	<div class="col-md-10 col-md-offset-1 mar-b-30">
 	<!-- 상단 광고 -->
-	<ins id="noadsense" class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9592402831871199" data-ad-slot="7142234264" data-ad-format="auto"></ins><br>
+	<ins id="noadsense" class="adsbygoogle" style="display:block;height:90px;" data-ad-client="ca-pub-6081569795236180" data-ad-slot="4545283356" data-ad-format="auto"></ins><br>
 	<!-- 상단 광고 끝 -->
 	<?php if ( $this->data['catlinks'] ) {
 	$this->html( 'catlinks' );
