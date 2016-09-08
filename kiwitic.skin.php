4a589e41764e9af3cf28de75c70163526aeb3cce
Cocoa <cocoa@kiwki.us>
2016-09-08 22:14:57 +0900

    대한위키실록과의 협력관계

--- kiwitic.skin.php
+++ kiwitic.skin.php
@@ -136,6 +136,7 @@ class KiwiticTemplate extends BaseTemplate {
 			<li id="t-re"><?php echo '<a href="//issues.kiwki.us">';?>키위위키 이슈 트래커</a></li>
 			<li id="t-re"><?php echo '<a href="//wikiwi.xyz">';?>키위위키 서버 상태</a></li>
 			<li id="t-re"><?php echo '<a href="//buswiki.ml">';?>버스위키</a></li>
+			<li id="t-re"><?php echo '<a href="//wsilog.ml">';?>대한위키실록</a></li>
 						</ul>
 				</li>
 				<li class="dropdown">
