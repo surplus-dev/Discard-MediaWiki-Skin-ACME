bda3a741de591232e4c3e9d9f396a528f2427ca7
Cocoa <cocoa@hoto.us>
2016-10-16 22:49:19 +0900

    버튼 고침

--- kiwitic.skin.php
+++ kiwitic.skin.php
@@ -240,7 +240,7 @@ class KiwiticTemplate extends BaseTemplate {
 	</div>
 	</section>
 	<div class="scroll-buttons">
-		<a class="scroll-button" href="javascript:void(0);"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
+		<a class="scroll-button" href="#"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
 		<a class="scroll-bottom" href="#footer"><i class="fa fa-arrow-down" aria-hidden="true"></i></a>
 	</div>
 	<!--small footer start -->
