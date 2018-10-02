abf9015929f88ef4d8642c27faf1a0e9d0ac9e23
PlavorSeol <pseol2190@outlook.com>
2018-10-02 17:34:45 +0900

    Drop support for MediaWiki 1.25 or older

--- acme.php
+++ acme.php
@@ -1,18 +1,5 @@
 <?php
-    if (!defined('MEDIAWIKI')) {
-        die("This is an extension to the MediaWiki package and cannot be run standalone.");
-    }
+if (!function_exists("wfLoadSkin"))
+{die("Acme skin requires MediaWiki 1.25 or newer.");}
 
-    $wgExtensionCredits['skin'][] = array(
-        'path'        => __FILE__,
-        'name'        => 'acme',
-        'url'         => 'https://shapebootstrap.net/item/1524925-acme-free-responsive-corporate-template/comments',
-        'author'      => 'cosmic 원본, 김동동 수정, [https://github.com/2DU/acme/graphs/contributors 기타]',
-        'description' => 'acme 부트스트랩 테마를 미디어위키에 적용합니다.',
-        'license-name' => "Free License",
-        'version' => '16-12-05 일반',
-    );
-
-    $wgValidSkinNames['acme'] = 'Acme';
-    $wgAutoloadClasses['SkinAcme'] = __DIR__ . '/acme.skin.php';
-?>
+wfLoadSkin("Acme");
